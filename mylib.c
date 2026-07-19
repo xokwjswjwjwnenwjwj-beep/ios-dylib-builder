647075a0284a5ceb46ed55a67c2397fce81f3012
xokwjswjwjwnenwjwj-beep <xokwjswjwjwnenwjwj@gmail.com>
2026-07-20 02:02:47 +0700

    Refactor mylib.c by removing unused functions
    Removed old hello function and add_numbers function.

--- mylib.c
+++ mylib.c
@@ -1,16 +1,5 @@
-C
-
-
-
-
 #include <stdio.h>
 
-// Hàm này dùng để in ra một dòng chữ chào mừng
-void hello_from_ios_dylib() {
-    printf("Xin chào! File dylib này đã được build thành công từ GitHub Actions!\n");
-}
-
-// Hàm này dùng để cộng 2 số nguyên với nhau
-int add_numbers(int a, int b) {
-    return a + b;
+void hello_from_dylib() {
+    printf("Hello from iOS Dylib!\n");
 }
